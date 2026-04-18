1807fae9ff7ac0e221c5e4a943a59b662914a666
s-moha092 <smoha380@my.centennialcollege.ca>
2026-04-18 11:55:54 -0400

    Initial class library with extension method

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace StringExtensionsLib;
+
+public static class StringExtensions
+{
+    public static bool StartsWithUpper(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return false;
+
+        return char.IsUpper(input[0]);
+    }
+}
